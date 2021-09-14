2efeeeffde1c11a6571a3c6505639e195f2d1038
BAA20 <BAA20@DESKTOP-1K2M970>
2021-09-14 16:25:23 +0300

    Добавьте файлы проекта.

---
+++ Library_system_GUI/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_system_GUI
+{
+    class Book
+    {
+        public int Id;
+        public string Author;
+        public string Name;
+        public Book(int id, string author, string name)
+        {
+            Id = id;
+            Author = author;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "Книга. ID: " + Id + " Автор: " + Author + " Название: " + Name;
+        }
+    }
+}
---
+++ Library_system_GUI/BookReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_system_GUI
+{
+    class BookReader
+    {
+        public string Name;
+        public int Year;
+        public const int maxBook = 10;
+        public List<Book> Books;
+        
+        public BookReader(string name, int  year)
+        {
+            Name = name;
+            Year = year;
+            Books = new List<Book>();
+        }
+        public bool TakeBook(Book bk)
+        {
+            if (Books.Count >= maxBook) return false;
+            Books.Add(bk);
+            return true;
+        }
+        public Book BookBack() 
+        {
+            if (Books.Count == 0) return null;
+            Book bk = Books[0];
+            Books.RemoveAt(0);
+            return bk;
+        }
+        public Book BookBack(int id)
+        {
+            if (Books.Count == 0) return null;
+            for (int i = 0; i<Books.Count; i++) 
+            {
+                if (Books[i].Id == id)
+                {
+                    Book bk = Books[i];
+                    Books.RemoveAt(i);
+                    return bk;
+                }
+            }
+            return null;
+        }
+        public List<Book> BackAll()
+        {
+            List<Book> bl;
+            bl = new List<Book>();
+            Book bk;
+            while ((bk = BookBack()) != null) bl.Add(bk);
+            return bl;
+        }
+        public override string ToString()
+        {
+            return "Читатель " + Name + " " + Year + " г." + "Книг на руках: " + Books.Count;
+        }
+    }
+}
